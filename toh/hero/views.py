684bddf2d441b9ab75db2ff29b4f0752d6267d7c
Seungwon Shin <namnamseo@snu.ac.kr>
2020-10-15 19:13:11 +0900

    Setup for Task #2

--- toh/hero/views.py
+++ toh/hero/views.py
@@ -1,10 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Hero
 
-def index(request):
-	return HttpResponse("Hello, world!")
-
-def hero_id(request, id_=""):
-	return HttpResponse(f"Your id is {id_}!")
-
-def hero_name(request, name=""):
-	return HttpResponse(f"Your name is {name}!")
+@csrf_exempt
+def hero_list(request):
+	if request.method == 'GET':
+		hero_all_list = [hero for hero in Hero.objects.all().values()]
+		return JsonResponse(hero_all_list, safe=False)
+	elif request.method == 'POST':
+		try:
+			body = request.body.decode()
+			hero_name = json.loads(body)['name']
+		except (KeyError, JSONDecodeError) as e:
+			return HttpResponseBadRequest()
+		hero = Hero(name=hero_name)
+		hero.save()
+		response_dict = {'id': hero.id, 'name': hero.name}
+		return JsonResponse(response_dict, status=201)
+	else:
+		return HttpResponseNotAllowed(['GET', 'POST'])
