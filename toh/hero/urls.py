14e02ac2715f6764de9216d22b69379e2ea7df05
Son Kangwon <fjqlddb06@snu.ac.kr>
2020-10-15 21:17:44 +0900

    re-Practice6

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    # path('<int:id>/', views.hero_id, name='hero_id'),
-    # path('<str:name>/', views.hero_name, name='hero_name'),
+    path('<int:id>/', views.hero_id, name='hero_id'),
+    path('<str:name>/', views.hero_name, name='hero_name'),
 
     path('', views.hero_list, name='hero_list'),
     path('info/<int:id>/', views.hero_one, name='hero_info')
