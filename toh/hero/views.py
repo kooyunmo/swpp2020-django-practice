228af43300f0d25c96f48fdd9494d3d69b2d0f90
koyrkr <koyrkr@gmail.com>
2020-10-15 19:17:31 +0900

    task 1

---
+++ toh/hero/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello, world!')
+
+def hero_id(request, id=0):
+    return HttpResponse("Your id is " + str(id) + "!")
+
+def hero_name(request, name=""):
+    return HttpResponse("Your name is " + name + "!")
