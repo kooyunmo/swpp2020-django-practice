c88047d21920b9c2365dd65e0e54ba92afd6945e
sc613 <sc613@snu.ac.kr>
2020-10-15 19:09:16 +0900

    Task 1

---
+++ toh/hero/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse('Hello, world!')
+
+def hero_id(request, id=0):
+  return HttpResponse('Your id is ' + str(id) + '!')
+
+def hero_name(request, name=""):
+  return HttpResponse('Your name is ' + name + '!')
