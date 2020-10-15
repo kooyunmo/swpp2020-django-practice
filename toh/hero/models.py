5f290d2dbc5c215668abb27364e75674a5c5bd21
Seungwon Shin <namnamseo@snu.ac.kr>
2020-10-15 19:45:10 +0900

    Add Hero model with name and age

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.db.models import CharField
+from django.db.models import CharField, IntegerField
 
 class Hero(models.Model):
 	name = CharField(max_length=120)
+	age = IntegerField(default=0)
 
 	def __str__(self):
-		return self.name
+		return f'<Hero name="{self.name}", age={self.age}>'
