654701b6a7952464976e506836761d42767c7717
koyrkr <koyrkr@gmail.com>
2020-10-16 03:41:26 +0900

    Task 2 & Task 3

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.hero_id),
-    path('<str:name>/', views.hero_name),
+    # path('', views.index, name='index'),
+    # path('<int:id>/', views.hero_id),
+    # path('<str:name>/', views.hero_name),
+    path('', views.hero_list),
+    path('info/<int:id>/', views.hero_info),
 ]
