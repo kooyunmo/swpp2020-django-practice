dedf7bfb0a508b7ad0eb647323d8a0a34f68a231
Jaewook Lee <jaewooklee@snu.ac.kr>
2020-10-16 20:44:08 +0900

    finish task 2

--- toh/hero/views.py
+++ toh/hero/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Hero
 # Create your views here.
+
+@csrf_exempt
+def hero_list(request):
+    if request.method == 'GET':
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == 'POST':
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)['name']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero(name=hero_name)
+        hero.save()
+        response_dict = {'id': hero.id, 'name': hero.name, 'age': hero.age}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+@csrf_exempt
+def hero_info(request, id):
+    if request.method == 'GET':
+        hero = Hero.objects.get(id=id)
+        response_dict = {'id': hero.id, 'name': hero.name, 'age': hero.age}
+        return JsonResponse(response_dict, status=201)
+    elif request.method == 'PUT':
+        try:
+            body = request.body.decode()
+            body = json.loads(body)
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero.objects.get(id=id)
+        hero.name = body['name']
+        hero.age = body['age']
+        hero.save()
+        response_dict = {'id': hero.id, 'name': hero.name, 'age': hero.age}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'PUT'])
+        
 
 def index(request):
     return HttpResponse('Hello, World!')
